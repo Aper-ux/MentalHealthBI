4e5e5810e1a2592a2e5740f913dc7248088c0b25
IsaiDiaz <jsoza057@gmail.com>
2023-05-10 15:51:27 -0400

    Correccion de errores y agregacion de nueva grafica

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ Plotly.d3.csv("https://raw.githubusercontent.com/Aper-ux/MentalHealthBI/main/Stu
             size: 0.5
           },
           marker: {
-            color: 'skyblue'
+            color: 'salmon'
           }
         }
       ];
@@ -137,5 +137,86 @@ Plotly.d3.csv("https://raw.githubusercontent.com/Aper-ux/MentalHealthBI/main/Stu
       Plotly.newPlot('grafico4', datos, configuracion);
       
     });
+    // Carga los datos del archivo CSV  
+    Plotly.d3.csv("https://raw.githubusercontent.com/Aper-ux/MentalHealthBI/main/Student%20Mental%20health.csv", function(err, data) {
 
+        // Convierte los datos de texto a numéricos
+        data.forEach(function(d) {
+            if (d.panic_attack == "No") {
+                d.panic_attack = 0;
+            }
+            else {
+                d.panic_attack = 1;
+            }
+            if (d.anxiety == "No") {
+                d.anxiety = 0;
+            }
+            else {
+                d.anxiety = 1;
+            }
+            if (d.depression == "No") {
+                d.depression = 0;
+            }
+            else {
+                d.depression = 1;
+            }
+            if (d.marital_status == "No"){
+                d.marital_status = 0;
+            }
+            else {
+                d.marital_status = 1;
+            }
+            if (d.gender == "Male"){
+                d.gender == 0;
+            }else{
+                d.gender == 1;
+            }
+        });
+
+        console.log(data);
+
+        // Prepara los datos para el gráfico
+        var datos = [
+            {
+                y: data.map(function(d) { return d.panic_attack; }),
+                x: data.map(function(d) { return d.gender; }),
+
+                type: 'bar',
+                name: 'Ataques de pánico',
+
+            },
+            {
+                y: data.map(function(d) { return d.anxiety; }),
+                x: data.map(function(d) { return d.gender; }),
+
+                type: 'bar',
+                name: 'Ansiedad',
+
+            },
+            {
+                y: data.map(function(d) { return d.depression; }),
+                x: data.map(function(d) { return d.gender; }),
+
+                type: 'bar',
+                name: 'Depresión',
+
+            }
+        ];
+
+        // Configura el diseño del gráfico
+        var configuracion = {
+            barmode: 'group',
+            title: 'Gráfico de dispersión de puntuaciones de ansiedad, depresión y ataques de pánico',
+            xaxis: {
+                title: 'Puntuación de ataques de pánico'
+            },
+            yaxis: {
+                title: 'Puntuación de ansiedad'
+            }
+        };
+
+        // Dibuja el gráfico
+        Plotly.newPlot('grafico6', datos, configuracion);
+
+    });
     
